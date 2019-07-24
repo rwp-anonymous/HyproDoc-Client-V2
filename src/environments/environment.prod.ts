a83ac14ab8318af38a0c4172e9cfbb88b4030454
Devin <devdhera@gmail.com>
2019-07-25 00:17:34 +0530

    Added envs

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://api.hyprodoc.com/api/v2',
+    fileUploadBaseUrl: 'https://storage.googleapis.com/hyprodoc/DEV/uploads'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'http://localhost:3000/api/v2',
+    fileUploadBaseUrl: 'https://storage.googleapis.com/hyprodoc/DEV/uploads'
+};
